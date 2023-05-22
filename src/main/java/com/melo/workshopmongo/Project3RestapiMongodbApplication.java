819e165879a0b19c2bf3571d87c734aeffaa8c22
andremelo97 <andre.melo9715@gmail.com>
2023-05-22 10:05:18 -0300

    first commit

---
+++ src/main/java/com/melo/workshopmongo/Project3RestapiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.melo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3RestapiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3RestapiMongodbApplication.class, args);
+	}
+
+}
