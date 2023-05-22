27348a333dda4e79d63731fb51e2b42641787b40
andremelo97 <andre.melo9715@gmail.com>
2023-05-22 17:03:26 -0300

    DTO author

--- src/main/java/com/melo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/melo/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.melo.workshopmongo.domain.Post;
 import com.melo.workshopmongo.domain.User;
+import com.melo.workshopmongo.dto.AuthorDTO;
 import com.melo.workshopmongo.repository.PostRepository;
 import com.melo.workshopmongo.repository.UserRepository;
 
@@ -34,11 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Vacation time", "I'm travelling to São Paulo, Brazil. See ya!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today!", maria);
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Vacation time", "I'm travelling to São Paulo, Brazil. See ya!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
